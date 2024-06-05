32bda11e8a8482f769913ca517833c221d3996f3
fercast711 <fercast7.fc@gmail.com>
2024-06-05 14:52:37 -0600

    first commit

---
+++ server/config.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const PORT = process.env.PORT
+export const USER = process.env.USER
+export const HOST = process.env.HOST 
+export const DATABASE = process.env.DATABASE
+export const PASSWORD = process.env.PASSWORD
+export const PORTDB = process.env.PORTDB
---
+++ server/index.js
@@ -0,0 +1,11 @@
+import app from "./app.js";
+import { PORT } from "./config.js";
+import pool from "./db.js";
+
+pool.connect().then(() => {
+  console.log('connect successful')
+}).catch((e) => {
+  console.log('error connecting', e.message)
+})
+
+app.listen(PORT, () => console.log(`Server listen on port ${PORT}`))
